9484a18ae530a230ec0be52fc740f90bcd90662e
aaronkho <aaronkho@mit.edu>
2026-02-25 17:06:37 -0500

    Improvements for surrogate example

--- Tutorial_3/gpytorch_surrogate.py
+++ Tutorial_3/gpytorch_surrogate.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+import numpy as np
 import pandas as pd
 import torch
 import gpytorch
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+
 import matplotlib.pyplot as plt
 
 
@@ -19,10 +22,10 @@ class ExampleGPModel(gpytorch.models.ExactGP):
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 
-input_vars = ['r', 'a', 'ip', 'bt', 'kappa', 'delta', 'zeffped', 'betan', 'neped']
+input_vars = ['r', 'a', 'ip', 'bt', 'kappa', 'delta', 'zeffped', 'betan', 'neped', 'nsfrac']
 output_vars = ['ptop']
-df = pd.read_hdf('sample_data_9d_surrogate.h5', key='/data')
-train_data, test_data = train_test_split(df, test_size=10, shuffle=False)
+df = pd.read_hdf('sample_data_10d_surrogate.h5', key='/data')
+train_data, test_data = train_test_split(df, test_size=100, shuffle=False)
 
 train_x = train_data[input_vars].to_numpy()
 train_y = train_data[output_vars].to_numpy().flatten()
@@ -41,7 +44,7 @@ model.train()
 likelihood.train()
 
 # Use the Adam optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=1.0)  # Includes GaussianLikelihood parameters
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
 
 # Marginal log likelihood loss function
 mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
@@ -65,5 +68,69 @@ for i in range(n_iterations):
 
 model.eval()
 likelihood.eval()
-score = r2_score(likelihood(model(tensor_test_x)).mean.detach().numpy(), test_y, multioutput='raw_values')
+tensor_check_y = likelihood(model(tensor_train_x))
+tensor_fit_y = likelihood(model(tensor_test_x))
+check_y = tensor_check_y.mean.detach().numpy()
+check_ye = np.sqrt(tensor_check_y.variance.detach().numpy())
+fit_y = tensor_fit_y.mean.detach().numpy()
+fit_ye = np.sqrt(tensor_fit_y.variance.detach().numpy())
+score = r2_score(fit_y, test_y, multioutput='raw_values')
 print(f'R2 score: {score}')
+
+
+plot_save_directory = Path('./gpytorch_10d_output')
+plot_save_directory.mkdir(parents=True, exist_ok=True)
+plot_sigma = 2.0
+
+# Raw data with GPR fit and error, only accounting for y-errors
+save_file1 = plot_save_directory / 'gpytorch_10d_surrogate_test.png'
+fig1 = plt.figure(1, figsize=(8, 6))
+ax1 = fig1.add_subplot(111)
+ax1.errorbar(train_y.flatten(), check_y.flatten(), yerr=plot_sigma * check_ye.flatten(), ls='', marker='+', color='b', label='Training')
+ax1.errorbar(test_y.flatten(), fit_y.flatten(), yerr=plot_sigma * fit_ye.flatten(), ls='', marker='s', color='r', label='Testing')
+ax1.annotate(r'$R^2_{test} = ' + f'{score[0]:.6f}' + r'$', xy=(50.0, 780.0), xytext=(50.0, 780.0))
+xmin, xmax = ax1.get_xlim()
+ymin, ymax = ax1.get_ylim()
+lmin = np.nanmin([xmin, ymin])
+lmax = np.nanmin([xmax, ymax])
+ax1.plot([lmin, lmax], [lmin, lmax], ls='--', c='k')
+ax1.set_xlim(lmin, lmax)
+ax1.set_ylim(lmin, lmax)
+ax1.legend(loc='best')
+fig1.savefig(save_file1)
+
+
+df_scan = pd.read_hdf('sample_test_10d_surrogate.h5', key='/data')
+scan_x = df_scan[input_vars].to_numpy()
+scan_y = df_scan[output_vars].to_numpy().flatten()
+scan_var = 'neped'
+scan_points = 101
+scan_range = abs(df_scan[scan_var].max() - df_scan[scan_var].min())
+smin = float(df_scan[scan_var].min() - 0.05 * scan_range)
+smax = float(df_scan[scan_var].max() + 0.05 * scan_range)
+scan = np.linspace(smin, smax, scan_points)
+scan_data = [np.full((scan_points, ), float(df_scan[key].mean())) for key in input_vars if key in df_scan]
+scan_data[input_vars.index(scan_var)] = scan.copy()
+scan_fit_x = np.stack(scan_data, axis=-1)
+tensor_scan_fit_x = torch.tensor(scan_fit_x)
+
+tensor_scan_fit_y = likelihood(model(tensor_scan_fit_x))
+scan_fit_y = tensor_scan_fit_y.mean.detach().numpy()
+scan_fit_ye = np.sqrt(tensor_scan_fit_y.variance.detach().numpy())
+
+# Raw data with GPR fit and error, only accounting for y-errors
+save_file2 = plot_save_directory / 'gpytorch_10d_surrogate_test_scan.png'
+fig2 = plt.figure(2, figsize=(8, 6))
+ax2 = fig2.add_subplot(111)
+plot_fit_y_lower = scan_fit_y - plot_sigma * scan_fit_ye
+plot_fit_y_upper = scan_fit_y + plot_sigma * scan_fit_ye
+ax2.scatter(df_scan[scan_var].to_numpy().flatten(), scan_y.flatten(), ls='', marker='o', color='k', label='Truth')
+ax2.plot(scan.flatten(), scan_fit_y.flatten(), color='r', label='Surrogate')
+ax2.fill_between(scan.flatten(), plot_fit_y_lower, plot_fit_y_upper, facecolor='r', edgecolor='None', alpha=0.2)
+ax2.set_xlim(smin, smax)
+ax2.legend(loc='best')
+fig2.savefig(save_file2)
+plt.close(fig2)
+
+
+plt.show()
